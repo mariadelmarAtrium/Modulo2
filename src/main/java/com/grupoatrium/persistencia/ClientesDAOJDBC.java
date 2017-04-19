0cdbbc0922baff6be68154e5274cfad2f3634871
maria garcia <mariadelmar@aprolivajaen.com>
2017-04-19 13:35:58 +0200

    Agregar DAOJDBC

---
+++ src/main/java/com/grupoatrium/persistencia/ClientesDAOJDBC.java
@@ -0,0 +1,11 @@
+package com.grupoatrium.persistencia;
+
+public class ClientesDAOJDBC implements ItfzDAO{
+	
+	@Override
+	public void insertar(Object object) {
+		// TODO Auto-generated method stub
+		System.out.println("Insertando JDBC");
+	}
+
+}
